6759faece4e51507ee406fe203d648078186a2d5
Krysta Gonzalez <gnosisknows28@gmail.com>
2023-10-02 18:53:01 -0500

    icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { GoVideo, GoPeople, GoFlame } from "react-icons/go";
 
 export default function Footer() {
   return (
     <footer>
       <ul>
-        <li><a href="https://github.com/Chongi23">My GitHub</a></li>
-        <li><a href="https://www.linkedin.com/in/krysta-gonzalez-web-developer">My LinkedIn</a></li>
-        <li><a href="https://www.youtube.com/channel/UC97mnXeHjH_hjzemTfukuiA">My YouTube</a></li>
+        <li><a href="https://github.com/Chongi23">GitHub<GoFlame/></a></li>
+        <li><a href="https://www.linkedin.com/in/krysta-gonzalez-web-developer"><GoPeople/></a></li>
+        <li><a href="https://www.youtube.com/channel/UC97mnXeHjH_hjzemTfukuiA"><GoVideo /></a>
+         </li>
       </ul>
     </footer>
   );
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -6,11 +6,10 @@ export default function Portfolio() {
       <h1>Portfolio</h1>
       <p>
         These are some portfolio projects to you can view!
-        <ul>
-        <a href='https://jacobdflores.github.io/Food-Finder/'>Food-Finder</a>
-        <a href='https://powerful-fjord-96372-1e2a1c430418.herokuapp.com/'>Pocket-chanj</a>
-        <a href='https://fitness-finesse-5b38e12b77f3.herokuapp.com/'>Fitness-Finesse</a>
-        </ul>
+        <li><a href='https://jacobdflores.github.io/Food-Finder/'>Food-Finder</a> </li>
+       <li> <a href='https://powerful-fjord-96372-1e2a1c430418.herokuapp.com/'>Pocket-chanj</a></li>
+       <li> <a href='https://fitness-finesse-5b38e12b77f3.herokuapp.com/'>Fitness-Finesse</a></li>
+       
       </p>
     </div>
   );
